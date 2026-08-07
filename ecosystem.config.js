3e7a2176552fef5610725c6294bc58b7e4fcd40a
Imran Khan <imranmodel35@gmail.com>
2026-08-07 20:25:02 +0600

    added pm2 support

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "agency-flow",
-      script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      name: "successor-crm",
+      script: "npm",
+      args: "start",
       cwd: __dirname,
-      instances: 1,
-      exec_mode: "fork",
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "512M",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
       },
+      autorestart: true,
+      max_restarts: 10,
+      watch: false,
     },
   ],
 };
